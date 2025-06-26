d7b0a5d831096eb522f2bfa30192effa7e883195
Angelo Theodorou <encelo@gmail.com>
2025-06-27 01:31:58 +0200

    Add atomic classes for unsigned integers
    - Add bitwise operations to the atomic classes
    - Change the signature of the atomic compare and exchange
    - Add the acquire release memory ordering semantics
    - Replace the atomic unit tests for a single typed one

--- unit_tests/gtest_atomic.h
+++ unit_tests/gtest_atomic.h
@@ -6,8 +6,63 @@
 
 namespace {
 
-const int NumThreads = 100;
-const int NumIterations = 1000;
+const unsigned int NumThreads = 100;
+const unsigned NumIterations = 1000;
+const unsigned int ArrayLength = 8;
+
+template <typename T>
+void printValue(T value) {}
+
+template <>
+void printValue<int32_t>(int32_t value)
+{
+	printf("%d\n", value);
+}
+
+template <>
+void printValue<uint32_t>(uint32_t value)
+{
+	printf("%u\n", value);
+}
+
+template <>
+void printValue<int64_t>(int64_t value)
+{
+	printf("%ld\n", value);
+}
+
+template <>
+void printValue<uint64_t>(uint64_t value)
+{
+	printf("%lu\n", value);
+}
+
+template <typename T>
+void printHexValue(T value) {}
+
+template <>
+void printHexValue<int32_t>(int32_t value)
+{
+	printf("%x\n", value);
+}
+
+template <>
+void printHexValue<uint32_t>(uint32_t value)
+{
+	printf("%x\n", value);
+}
+
+template <>
+void printHexValue<int64_t>(int64_t value)
+{
+	printf("%lx\n", value);
+}
+
+template <>
+void printHexValue<uint64_t>(uint64_t value)
+{
+	printf("%lx\n", value);
+}
 
 }
 
