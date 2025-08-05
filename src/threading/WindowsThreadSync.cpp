dcbb322ad80b4ecc1d3271362d6a983b0cfe7552
Angelo Theodorou <encelo@gmail.com>
2025-08-05 15:48:18 +0200

    Add kernel and user-space semaphore classes
    - Add code to detect the number of processors on Emscripten
    - Add a lock guard class to wrap a mutex in a scoped block
    - Define _WIN32_WINNT in the common Windows defines
    - Change the API call to detect the number of processors on Windows
    - Change the API call to yield a thread execution on Windows
    - Link to the synchronization library on Windows

--- src/threading/WindowsThreadSync.cpp
+++ src/threading/WindowsThreadSync.cpp
@@ -93,4 +93,136 @@ void CondVariable::broadcast()
 	WakeAllConditionVariable(&handle_);
 }
 
+///////////////////////////////////////////////////////////
+// Semaphore CLASS
+///////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////
+// CONSTRUCTORS and DESTRUCTOR
+///////////////////////////////////////////////////////////
+
+Semaphore::Semaphore()
+    : Semaphore(0)
+{
+}
+
+Semaphore::Semaphore(unsigned int initialCount)
+{
+	// Setting `LONG_MAX` as the maximum value, effectively unlimited for typical uses
+	handle_ = CreateSemaphore(nullptr, initialCount, 0x7FFFFFFF, nullptr);
+}
+
+Semaphore::~Semaphore()
+{
+	CloseHandle(handle_);
+}
+
+///////////////////////////////////////////////////////////
+// PUBLIC FUNCTIONS
+///////////////////////////////////////////////////////////
+
+void Semaphore::wait()
+{
+	WaitForSingleObject(handle_, INFINITE);
+}
+
+void Semaphore::signal()
+{
+	ReleaseSemaphore(handle_, 1, nullptr);
+}
+
+bool Semaphore::tryWait()
+{
+	const DWORD result = WaitForSingleObject(handle_, 0);
+	return (result == WAIT_OBJECT_0);
+}
+
+void Semaphore::wait(unsigned int count)
+{
+	for (unsigned int i = 0; i < count; i++)
+		WaitForSingleObject(handle_, INFINITE);
+}
+
+void Semaphore::signal(unsigned int count)
+{
+	ReleaseSemaphore(handle_, count, nullptr);
+}
+
+#if HAVE_USER_SEMAPHORE
+///////////////////////////////////////////////////////////
+// UserSemaphore CLASS
+///////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////
+// CONSTRUCTORS and DESTRUCTOR
+///////////////////////////////////////////////////////////
+
+UserSemaphore::UserSemaphore()
+    : UserSemaphore(0)
+{
+}
+
+UserSemaphore::UserSemaphore(int initialCount)
+    : count_(initialCount)
+{
+}
+
+///////////////////////////////////////////////////////////
+// PUBLIC FUNCTIONS
+///////////////////////////////////////////////////////////
+
+void UserSemaphore::wait()
+{
+	const int32_t c = count_.fetchSub(1, nctl::MemoryModel::ACQUIRE);
+	if (c <= 0)
+	{
+		int32_t expected = c - 1;
+		while (count_.load(nctl::MemoryModel::RELAXED) < 0)
+		{
+			WaitOnAddress(&count_, &expected, sizeof(expected), INFINITE);
+			expected = count_.load(nctl::MemoryModel::RELAXED);
+		}
+	}
+}
+
+void UserSemaphore::signal()
+{
+	const int32_t c = count_.fetchAdd(1, nctl::MemoryModel::RELEASE);
+	if (c < 0)
+		WakeByAddressSingle(&count_);
+}
+
+bool UserSemaphore::tryWait()
+{
+	int32_t c = count_.load(nctl::MemoryModel::RELAXED);
+	while (c > 0)
+	{
+		if (count_.cmpExchange(c, c - 1, nctl::MemoryModel::ACQUIRE))
+			return true;
+	}
+	return false;
+}
+
+void UserSemaphore::wait(unsigned int count)
+{
+	const int32_t c = count_.fetchSub(count, nctl::MemoryModel::ACQUIRE);
+	if (c < static_cast<int32_t>(count))
+	{
+		int32_t expected = count_.load(nctl::MemoryModel::RELAXED);
+		while (count_.load(nctl::MemoryModel::RELAXED) < 0)
+		{
+			WaitOnAddress(&count_, &expected, sizeof(expected), INFINITE);
+			expected = count_.load(nctl::MemoryModel::RELAXED);
+		}
+	}
+}
+
+void UserSemaphore::signal(unsigned int count)
+{
+	const int32_t c = count_.fetchAdd(count, nctl::MemoryModel::RELEASE);
+	if (c < 0)
+		WakeByAddressAll(&count_);
+}
+#endif
+
 }
